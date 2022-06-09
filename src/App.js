c31bde35f0ba4d7a50fae4d4c4e8cb0fb0209aa5
ale <ale@ale.com>
2022-06-09 20:45:58 -0300

    feat: created routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { useTheme } from "./Providers/Theme";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./Theme";
 
-import Header from "./components/Header";
-import { useTheme } from "./Providers/Theme";
 import { ContainerContent } from "./style/style";
+
 import Navegation from "./components/Navegaion";
-import IntroductionHome from "./components/IntroductionHome";
-import OrderCredit from "./components/OrderCredit";
+import RoutePage from "./Routes";
+import Header from "./components/Header";
 
 function App() {
   const { mode } = useTheme()
@@ -17,10 +17,7 @@ function App() {
         <Header />
         <Navegation />
 
-        <main>
-          <IntroductionHome />
-          <OrderCredit />
-        </main>
+        <RoutePage />
       </ContainerContent>
     </ThemeProvider>
   );
