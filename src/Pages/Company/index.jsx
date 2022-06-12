937099ce5b177e0a819f2c9523b6c4d01e56388d
ale <ale@ale.com>
2022-06-11 21:30:56 -0300

    feat: created page Company

---
+++ src/Pages/Company/index.jsx
@@ -0,0 +1,13 @@
+import BuildCompany from "../../components/BuildCompany";
+import IntroductionCompany from "../../components/IntoductionComp";
+
+function CompanyPage() {
+  return (
+    <main>
+      <IntroductionCompany />
+      <BuildCompany />
+    </main>
+  );
+}
+
+export default CompanyPage;
